fc3af003695cf1d7e0016e1c017dd9635087f8ab
Tokakun073 <13070221703@163.com>
2025-09-20 10:58:51 +0800

    增加打印_1

--- main.c
+++ main.c
@@ -5,8 +5,6 @@ int a[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
 int main()
 {
-    int *p = a;
-    printf("%d,p=%p , a=%p\n",a[p-a],p,a);
-    
-
+    printf("hello_world\n");
+    printf("1111122222\n");
 }
